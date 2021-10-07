31693cd5388377a6ab1e712deac64672f47a560f
vlad-moysieienko <moysieienko03@gmail.com>
2021-10-08 02:03:15 +0300

    Updates

---
+++ precache-manifest.cbfcec1f8d4a9bec3a4ba0d9cb626b88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d04da25e788e2a44d8e43c0df069bd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6117aa3cd3e63206cb8c",
+    "url": "./static/css/main.38d695d3.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6117aa3cd3e63206cb8c",
+    "url": "./static/js/main.f9daaf4a.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
